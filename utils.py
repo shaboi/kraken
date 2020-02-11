63912ec03dfd7e914a09516cdd232a93f4ada607
Adam <shaboi@users.noreply.github.com>
2020-02-11 09:58:27 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from typing import List
 
 def format_query(cols: List[str], values: List[str]):
     cols, values = str(tuple(cols)), str(tuple(values))
-    return f'INSERT INTO Trading.crypto.GO_KRAKEN_SETTLEMENTS {cols} VALUES {values}'
+    return f'INSERT INTO [TABLE] {cols} VALUES {values}'
 
 
-latest_record_dt = 'SELECT DATE_TIME FROM Trading.crypto.GO_KRAKEN_SETTLEMENTS ORDER BY DATE_TIME DESC LIMIT 1'
+latest_record_dt = 'SELECT DATE_TIME FROM [TABLE] ORDER BY [COL] DESC LIMIT 1'
